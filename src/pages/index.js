8fcb8930cdfc1d411d1fdbe7dbb7a6af0ac143f5
An <92383919+shifenok@users.noreply.github.com>
2021-10-18 17:51:20 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
+    <h2>Hi people2</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
